8b44d17fe801b94003b3e6696783fd13e92a46c1
anshulghogre4 <anshulghogre4@gmail.com>
2022-12-09 01:59:21 +0530

    added 0.0.0.0 to app.listen 3.0

--- backend/Server.js
+++ backend/Server.js
@@ -2,7 +2,7 @@
 const app = require("./app");
   const PORT = process.env.PORT || 4000 ;
 
-app.listen( PORT,'0.0.0.0',()=>{
+app.listen( PORT,`0.0.0.0:${PORT}`,()=>{
 
     console.log(`App is listening at: http://localhost:${PORT}`);
 
