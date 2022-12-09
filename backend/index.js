334298ee30bb54380a272631a5cad080c2a3241c
anshulghogre4 <anshulghogre4@gmail.com>
2022-12-09 23:50:05 +0530

    trying to fix railway hosting

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-
+require("dotenv").config();
 const app = require("./app");
   const PORT = process.env.PORT || 4000 ;
 
-app.listen( PORT,`0.0.0.0:${PORT}`,()=>{
+app.listen( PORT,()=>{
 
     console.log(`App is listening at: http://localhost:${PORT}`);
 
